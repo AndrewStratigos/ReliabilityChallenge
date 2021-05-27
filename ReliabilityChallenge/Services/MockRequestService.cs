227cd354de0c684943d4311c1106d67d52337c7a
Andrew Stratigos <astratigos@corum.com.au>
2021-05-27 16:51:53 +1000

    Add project files.

---
+++ ReliabilityChallenge/Services/MockRequestService.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using ReliabilityChallenge.Models;
+using System.Threading;
+
+namespace ReliabilityChallenge.Services
+{
+    public class MockRequestService
+    {
+        private IMediator mediator;
+
+        public MockRequestService(IMediator mediator)
+        {
+            this.mediator = mediator;
+            GenerateRequests();
+        }
+
+        private async void GenerateRequests() {
+            for (int i = 1; i <= 10; i++)
+            {
+                Thread.Sleep(1000);
+                await mediator.Send(new Request() { Id = i });
+            }
+        }
+    }
+}
